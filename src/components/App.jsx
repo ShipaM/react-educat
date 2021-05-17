6427f1ea8640d71da0a9a71ac996253e5da1620c
ShipaM <shipinomaksim@gmail.com>
2021-05-17 06:33:44 +0300

    addMovie

---
+++ src/components/App.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { moviesData } from "../moviesData";
+import MovieItem from "./MovieItem";
+
+//UI = fn(state,props)
+console.log(moviesData);
+
+// function removeMovie(movie) {
+//   const updateMovies = this.state.movies.filter(function(item){
+//     return item.id !== movie.id;
+//   });
+//   console.log(updateMovies);
+
+//   // this.state.movies=updateMovies;
+//   this.setState({
+//     movies: updateMovies
+//   });
+// }
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: moviesData,
+      moviesWillWatch: []
+    };
+  }
+
+  removeMovie = (movie) => {
+    const updateMovies = this.state.movies.filter(function (item) {
+      return item.id !== movie.id;
+    });
+    console.log(updateMovies);
+
+    // this.state.movies=updateMovies;
+    this.setState({
+      movies: updateMovies
+    });
+  };
+
+  addMovieToWillWatch = (movie) => {
+    console.log(movie)
+    const updateMovies = [...this.state.moviesWillWatch];
+    updateMovies.push(movie);
+
+    console.log(updateMovies)
+
+    this.setState({moviesWillWatch:updateMovies})
+  }
+
+
+
+  render() {
+    console.log(this);
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-9">
+            <div className="row">
+              {this.state.movies.map((movie) => {
+                return (
+                  <div className="col-6 mb-4" key={movie.id}>
+                    <MovieItem
+                      movie={movie}
+                      removeMovie={this.removeMovie}
+                      addMovieToWillWatch={this.addMovieToWillWatch}
+                    />
+                  </div>
+
+                );
+              })}
+            </div>
+
+          </div>
+
+          <div className="col-3">
+            <p>Will watch {this.state.moviesWillWatch.length}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+// function App() {
+//   return <div>{moviesData[0].title}</div>;
+// }
+
+export default App;
