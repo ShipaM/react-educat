311393a260fa76800460b7fef3f4130a804e30b1
ShipaM <shipinomaksim@gmail.com>
2021-05-23 20:56:19 +0300

    Updates

---
+++ precache-manifest.c626a2d50ccf42559a1d8a821e4e62ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d9391fcddbaa1d63bc4",
+    "url": "/ShipaM/react-educat/static/js/main.43535583.chunk.js"
+  },
+  {
+    "revision": "e7accc99f1eed08c1f1f",
+    "url": "/ShipaM/react-educat/static/js/runtime~main.752d77b5.js"
+  },
+  {
+    "revision": "f828632a24c69a4a3423",
+    "url": "/ShipaM/react-educat/static/css/2.f5c52f1b.chunk.css"
+  },
+  {
+    "revision": "f828632a24c69a4a3423",
+    "url": "/ShipaM/react-educat/static/js/2.31a2be60.chunk.js"
+  },
+  {
+    "revision": "732b291fb5fee8d05ceda55ba144c0db",
+    "url": "/ShipaM/react-educat/index.html"
+  }
+];
